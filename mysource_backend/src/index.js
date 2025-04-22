a06864006d8131b9ea59a71bd58da8cf2cf93bb3
Irehh <irechukwuchukwuka@gmail.com>
2025-04-22 13:24:09 +0100

    backend cicd 2

--- mysource_backend/src/index.js
+++ mysource_backend/src/index.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "https://mysource.com.ng" || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
