5e5a29dc34349b81890aa74e27069d542889192c
Irehh <irechukwuchukwuka@gmail.com>
2025-04-25 23:20:10 +0100

    front 2

--- mysource_backend - Copy/prisma/migrations/20250326062919_keyword_notification/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `notificationKeywords` VARCHAR(191) NULL;
--- mysource_backend - Copy/src/db.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-
-// Create a single instance of PrismaClient
-const prisma = new PrismaClient({
-  log: ["error", "warn"],
-})
-
-export default prisma
-
